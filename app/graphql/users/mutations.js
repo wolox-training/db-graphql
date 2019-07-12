9c4d7824b73ebdfcdd077e938469456892e4455f
Daniel Benavides <daniel.benavides@wolox.co>
2019-07-12 17:52:25 -0500

    Requested changes. Refactored helpers, deleted example code

--- app/graphql/users/mutations.js
+++ app/graphql/users/mutations.js
@@ -1,25 +1,14 @@
 const { gql } = require('apollo-server');
 
 const resolvers = require('./resolvers');
-const { userLoggedIn } = require('../events');
 
 module.exports = {
   mutations: {
-    createUser: resolvers.createUser,
-    login: (_, { credentials }) => {
-      // IMPORTANT: Not a functional login, its just for illustrative purposes
-      userLoggedIn.publish(credentials.username);
-      return {
-        accessToken: 'example_token',
-        refreshToken: 'example_refresh_token',
-        expiresIn: 134567899123
-      };
-    }
+    createUser: resolvers.createUser
   },
   schema: gql`
     extend type Mutation {
       createUser(user: UserInput!): User!
-      login(credentials: LoginInput!): AccessToken
     }
   `
 };
--- app/helpers/users.js
+++
@@ -1,5 +0,0 @@
-const bcrypt = require('bcryptjs');
-
-const config = require('../../config').common.session;
-
-exports.encodePassword = password => bcrypt.hash(password, parseInt(config.saltRounds));
