a89d6716f3d0d0eaff2c6dfc053fcb65f71a22d2
Daniel Benavides <daniel.benavides@wolox.co>
2019-07-16 15:08:07 -0500

    Refactored migration to be transactional

--- migrations/migrations/20190715152021-modify-users.js
+++ migrations/migrations/20190715152021-modify-users.js
@@ -2,23 +2,50 @@
 
 module.exports = {
   up: (queryInterface, Sequelize) =>
-    Promise.all([
-      queryInterface.renameColumn('users', 'firstName', 'first_name'),
-      queryInterface.renameColumn('users', 'lastName', 'last_name'),
-      queryInterface.removeColumn('users', 'username'),
-      queryInterface.changeColumn('users', 'created_at', { type: Sequelize.DATE, allowNull: false }),
-      queryInterface.changeColumn('users', 'updated_at', { type: Sequelize.DATE, allowNull: false })
-    ]),
+    queryInterface.sequelize.transaction(transaction =>
+      queryInterface
+        .renameColumn('users', 'firstName', 'first_name', { transaction })
+        .then(() => queryInterface.renameColumn('users', 'lastName', 'last_name', { transaction }))
+        .then(() => queryInterface.removeColumn('users', 'username', { transaction }))
+        .then(() =>
+          queryInterface.changeColumn(
+            'users',
+            'created_at',
+            { type: Sequelize.DATE, allowNull: false },
+            { transaction }
+          )
+        )
+        .then(() =>
+          queryInterface.changeColumn(
+            'users',
+            'updated_at',
+            { type: Sequelize.DATE, allowNull: false },
+            { transaction }
+          )
+        )
+    ),
   down: (queryInterface, Sequelize) =>
-    Promise.all([
-      queryInterface.renameColumn('users', 'first_name', 'firstName'),
-      queryInterface.renameColumn('users', 'last_name', 'lastName'),
-      queryInterface.addColumn('users', 'username', {
-        type: Sequelize.STRING,
-        allowNull: false,
-        unique: true
-      }),
-      queryInterface.changeColumn('users', 'created_at', { type: Sequelize.DATE }),
-      queryInterface.changeColumn('users', 'updated_at', { type: Sequelize.DATE })
-    ])
+    queryInterface.sequelize.transaction(transaction =>
+      queryInterface
+        .renameColumn('users', 'first_name', 'firstName', { transaction })
+        .then(() => queryInterface.renameColumn('users', 'last_name', 'lastName', { transaction }))
+        .then(() =>
+          queryInterface.addColumn(
+            'users',
+            'username',
+            {
+              type: Sequelize.STRING,
+              allowNull: false,
+              unique: true
+            },
+            { transaction }
+          )
+        )
+        .then(() =>
+          queryInterface.changeColumn('users', 'created_at', { type: Sequelize.DATE }, { transaction })
+        )
+        .then(() =>
+          queryInterface.changeColumn('users', 'updated_at', { type: Sequelize.DATE }, { transaction })
+        )
+    )
 };
