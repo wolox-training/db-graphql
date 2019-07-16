b3a5d04e078bab492b8dbb1cc8514f05b123320e
Daniel Benavides <daniel.benavides@wolox.co>
2019-07-16 11:44:09 -0500

    Refactored code, improved error handling on invalid input

--- app/graphql/users/middlewares.js
+++ app/graphql/users/middlewares.js
@@ -1,19 +1,26 @@
 const errors = require('../../errors');
-
-const emailRegex = /^[a-zñ0-9.+_-]+@(wolox.(ar|cl|co|com|com.ar))$/i;
-const passwordRegex = /^[a-zñ0-9._-]{8,}$/i;
+const { EMAIL_REGEX, PASSWORD_REGEX } = require('../../helpers/constants');
 
 exports.createUser = (resolve, root, args) => {
   const { user } = args;
   const errorList = [];
-  if (!emailRegex.test(user.email)) {
-    errorList.push('email must be a valid one in the Wolox domains');
+  if (!EMAIL_REGEX.test(user.email)) {
+    errorList.push({
+      field: 'email',
+      message: 'email must be a valid one in the Wolox domains',
+      statusCode: 422
+    });
   }
-  if (!passwordRegex.test(user.password)) {
-    errorList.push('password must be alphanumeric and have more than 8 characters');
+  if (!PASSWORD_REGEX.test(user.password)) {
+    errorList.push({
+      field: 'password',
+      message: 'password must be alphanumeric and have more than 8 characters',
+      statusCode: 422
+    });
   }
+
   if (errorList.length > 0) {
-    throw errors.badRequest(errorList.join(', '));
+    throw errors.invalidInputError('Invalid input values', errorList);
   }
   return resolve(root, args);
 };
